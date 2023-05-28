55d9971c32f9c976ed49f12ed65574e5c6b5e9b6
Thomas Honeyman <hello@thomashoneyman.com>
2023-05-28 18:27:44 -0400

    Pass through nodejs arg to spago-next

--- spago-next/default.nix
+++ spago-next/default.nix
@@ -1,3 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> {},
+  system ? builtins.currentSystem,
+  nodejs ? pkgs."nodejs-14_x",
+}:
 
-(import ./composition.nix { inherit pkgs; })."spago-next"
+(import ./composition.nix {inherit pkgs;})."spago-next"
